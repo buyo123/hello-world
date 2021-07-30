5c0cdeddf657d992feb75f19454dabe826b6eb15
Yutaro Yoshii <yakumodanshi@gmail.com>
2021-07-30 13:01:11 +0900

    practice

---
+++ for.js
@@ -0,0 +1,15 @@
+
+//example1
+for(let i = 0; i < 20; i++){
+    console.log(i+"歳になったけどまだクソババア死ねって思ってる");
+}
+
+//fibonacci
+//練習問題: フィボナッチ数列を出力するコードを書いて
+console.log("フィボナッチ数列を計算中...");
+let fib = 1;
+let prev = 1;
+for(let i = 0; i < 10; i++){
+    console.log(fib);
+}
+console.log("フィボナッチ数列計算完了!");
