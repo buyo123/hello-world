bec522870d3695e15af70ae9a214b0ab015d8da0
Yutaro Yoshii <yakumodanshi@gmail.com>
2021-07-30 14:07:23 +0900

    added a practice problem

---
+++ fizbuz.js
@@ -0,0 +1,50 @@
+// fizzbuzz.js
+//
+// rule:
+// write "Fizz Buzz" when a number is divisible both by 3 and 5
+// write "Fizz" when a number is divisible by 3
+// write "Buzz" when a number is divisible by 5 
+// 
+// to see if a number n is divisible by 5:
+// if(n%5 === 0){
+//     //write your code
+// }
+// % is the remainder division. 7%3 === 1
+//
+// output example
+// 1
+// 2
+// Fizz
+// 4
+// Buzz
+// Fizz
+// 7
+// 8
+// Fizz
+// Buzz
+// 11
+// Fizz
+// 13
+// 14
+// Fizz Buzz
+// 16
+// 17
+// Fizz
+// 19
+// Buzz
+// Fizz
+// 22
+// 23
+// Fizz
+// Buzz
+// 26
+// Fizz
+// 28
+// 29
+// Fizz Buzz
+// 31
+// 32
+// Fizz
+// 34
+// Buzz
+// Fizz
